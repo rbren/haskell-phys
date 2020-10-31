369482204cbd3055e4c841c267eacfccf9859364
Robert Brennan <contact@rbren.io>
2020-10-31 03:07:27 +0000

    add gifs

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 stack build
 stack exec hs-phys-exe
 ```
+
+```
+stack build && stack exec hs-phys-exe -- -o out.gif -w 400
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
 module Main where
 
 import Data.Typeable
 import Lib.SpaceVec
 import Lib.System
+import Lib.Particle
 
-sys = massOnASpring 9.0 1.0 1.0
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Prelude
+
+sys = massOnASpring 90.0 1.0 1.0
 
 delta = 0.01
 endTime = 10
@@ -15,4 +23,19 @@ steps = 500
 solution = iterate (eulerStep delta) sys
 systemSteps = take steps solution
 
-main = mapM_ print systemSteps
+getCircle :: System -> Diagram B
+--getCircle sys = circle (((time (state sys)) + 1.0) * 1.0) # lc white <> square 4 # lw none
+getCircle sys = circle 0.1 # lc white <> square 4 # lw none # translate (r2 (xLoc, yLoc))
+  where
+    p = pos (particle (state sys))
+    xLoc = x p
+    yLoc = y p
+
+--getCircle sys = circle 1 # lc white <> square 4 # lw none
+
+gif :: [(Diagram B, Int)]
+gif = map (\x -> (getCircle x, 1)) systemSteps
+
+--main = mapM_ print systemSteps
+main = mainWith gif
+
