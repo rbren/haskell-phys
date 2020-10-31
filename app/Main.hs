f7ddfb2d755181beaf9bb5d6e04de69dad715efa
Robert Brennan <contact@rbren.io>
2020-10-31 16:47:02 +0000

    add time label

--- app/Main.hs
+++ app/Main.hs
@@ -20,24 +20,35 @@ delta = 0.01
 endTime = 10
 startTime = time (state sys)
 --steps = toInteger ((endTime - startTime) / delta)
-steps = 500
+steps = 100
 
 solution = iterate (rk4Step delta) sys
 systemSteps = take steps solution
 
 getCircle :: System -> Diagram B
---getCircle sys = circle (((time (state sys)) + 1.0) * 1.0) # lc white <> square 4 # lw none
-getCircle sys = circle 0.1 # lc white <> square 4 # lw none # translate (r2 (xLoc, yLoc))
+getCircle sys = circle 0.1 # lc white # translate (r2 (xLoc, yLoc))
   where
     p = pos (particle (state sys))
     xLoc = x p
     yLoc = y p
 
---getCircle sys = circle 1 # lc white <> square 4 # lw none
+getLabel :: System -> Diagram B
+getLabel sys = text label # fontSizeL 0.1 # fc white # alignBL
+  where
+    t = time (state sys)
+    bump = t * 1000
+    rounded = fromIntegral (round bump :: Int)
+    unbump = rounded / 1000.0
+    label = "t=" ++ show unbump
 
 gif :: [(Diagram B, Int)]
-gif = map (\x -> (getCircle x, 1)) systemSteps
+gif = map (\sys -> (getSystemDiagram sys, 0)) systemSteps
+  where
+    origin = p2 (0.0, 0.0)
+    bl = p2 (-1.8, -1.8)
+    box = square 4 # lw 0.5 # lc white
+    getSystemDiagram :: System -> Diagram B
+    getSystemDiagram sys = position [(origin, getCircle sys), (bl, getLabel sys)] <> box
 
---main = mapM_ print systemSteps
 main = mainWith gif
 
